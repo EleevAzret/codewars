fce55bd6d04703fe2760f6e44a2a97bf2223889c
Азрет Елеев <azreteleev@MacBook-Air-Azret.local>
2023-01-22 23:43:01 +0300

    int32ToIPv4 is ready

---
+++ int32ToIPv4.js
@@ -0,0 +1,8 @@
+/**
+ * Function that takes an unsigned 32 bit number and returns a string representation of its IPv4 address.
+ * @param {Number} int32 
+ * @returns ready IPv4 address
+ */
+function int32ToIp(int32){
+  return `${(int32 >> 24) & 0xFF}.${(int32 >> 16) & 0xFF}.${(int32 >> 8) & 0xFF}.${(int32) & 0xFF}`;
+}
